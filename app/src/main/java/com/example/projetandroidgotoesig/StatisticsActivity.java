0847412182aa4ea598413e583309cc223001b469
PierreJeanLievens <pierre-jean.lievens@groupe-esigelec.org>
2024-12-14 23:25:22 +0100

    Ajout conso carbone

--- app/src/main/java/com/example/projetandroidgotoesig/StatisticsActivity.java
+++ app/src/main/java/com/example/projetandroidgotoesig/StatisticsActivity.java
@@ -37,6 +37,7 @@ public class StatisticsActivity extends AppCompatActivity {
         // Récupérer les références aux éléments UI pour afficher les statistiques
         TextView tripsCountTextView = findViewById(R.id.tripsCountTextView);
         TextView totalAmountTextView = findViewById(R.id.totalAmountTextView);
+        TextView totalCarbonTextView = findViewById(R.id.totalCarbonTextView);
 
         // Récupérer la référence à l'utilisateur dans la collection "user"
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -49,7 +50,7 @@ public class StatisticsActivity extends AppCompatActivity {
                 .addOnSuccessListener(queryDocumentSnapshots -> {
                     int tripsCount = 0;  // Compteur de trajets
                     double totalAmount = 0.0;  // Total des montants encaissés
-
+                    double distanceTotal = 0.0; // Total des distances
                     // Parcourir tous les trajets de l'utilisateur
                     for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
 
@@ -62,10 +63,21 @@ public class StatisticsActivity extends AppCompatActivity {
                         if (date != null && date.before(new Date())) {  // Vérifier si la date est passée
                             // Récupérer le prix
                             Double price = document.getDouble("price");
-                            if (price != null) {
-                                // Récupérer le tableau seatsBooked
-                                List<?> seatsBooked = (List<?>) document.get("seatsBooked");
-                                if (seatsBooked != null) {
+
+                            // Récupérer la distance
+                            Double distance = document.getDouble("distance");
+
+                            // Récupérer le tableau seatsBooked
+                            List<?> seatsBooked = (List<?>) document.get("seatsBooked");
+
+                            if (seatsBooked != null) {
+
+                                if (distance != null) {
+                                    // Calculer la distance totale pour ce trajet
+                                    distanceTotal += distance * seatsBooked.size();
+                                }
+
+                                if (price != null) {
                                     // Calculer le montant total pour ce trajet
                                     totalAmount += price * seatsBooked.size();  // Multiplier le prix par le nombre de places réservées
                                 }
@@ -73,9 +85,15 @@ public class StatisticsActivity extends AppCompatActivity {
                         }
                     }
 
+                    // Calculer l'émission carbone
+                    Double carbone =  distanceTotal * (6.5/100)*2.31;
+                    // Formater l'émission carbone avec 2 décimales
+                    String carboneFormate = String.format("%.2f", carbone);
+
                     // Afficher le nombre de trajets et le total des montants
                     tripsCountTextView.setText("Nombre de trajets créés : " + tripsCount);
                     totalAmountTextView.setText("Total des gains: " + totalAmount + " €");
+                    totalCarbonTextView.setText("Emissions carbones économisés: " + carboneFormate + " kg CO₂ ");
                 })
                 .addOnFailureListener(e -> {
                     Toast.makeText(StatisticsActivity.this, "Erreur lors de la récupération des trajets : " + e.getMessage(), Toast.LENGTH_SHORT).show();
